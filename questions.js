63eba0643196d0b4c912abc1cc6902d700671422
MSharfan <96681732+MSharfan@users.noreply.github.com>
2022-02-19 09:22:48 +0530

    Added Pages

---
+++ questions.js
@@ -0,0 +1,70 @@
+let questions = [
+    {
+    numb: 1,
+    question: "How many verse are there in Surah Mulk?",
+    answer: "30 verse",
+    options: [
+      "96 verse",
+      "200 verse",
+      "30 verse",
+      "83 verse"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who is the son of Zakariya A.S is?",
+    answer: "Prophet Yahya (PBUH)",
+    options: [
+      "Prophet Hud (PBUH)",
+      "Prophet Moosa (PBUH)",
+      "Prophet Lut (PBUH)",
+      "Prophet Yahya (PBUH)"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Surah/Chapter number for Surah Yusuf is?",
+    answer: "12",
+    options: [
+      "11",
+      "12",
+      "14",
+      "17"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Surah which comes after Surah An Nisa?",
+    answer: "Surah Al Maidah",
+    options: [
+      "Surah Al Maidah",
+      "Surah Al Imaran",
+      "Surah Al Araf",
+      "Surah Al Anfal"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Prophet Ismail A.S brother among the option?",
+    answer: "Prophet Isaac (PBUH)",
+    options: [
+      "Prophet Isa (PBUH)",
+      "Prophet Moosa (PBUH)",
+      "Prophet Ibrahim (PBUH)",
+      "Prophet Isaac (PBUH)"
+    ]
+  }
+    //  can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
